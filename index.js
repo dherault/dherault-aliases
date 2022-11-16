dec736cbb4b58775380c8d0d855eabbfa567b1e7
David Hérault <dherault@gmail.com>
2022-11-16 20:55:07 +0100

    Proof of concept

---
+++ index.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+const fs = require('node:fs')
+const path = require('node:path')
+const { execSync } = require('node:child_process')
+
+const aliases = `
+function commit() {
+  git add . -A
+
+  if [[ -z "$1" ]]; then
+    git commit
+  else
+    git commit -m "$*"
+  fi
+}
+
+function amend() {
+  git add . -A
+  git commit --amend --no-edit
+}
+
+alias amendx="git commit --amend"
+alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gpf="git push --force-with-lease"
+alias gp="git push"
+alias gs="git status"
+alias n="npm start"
+alias d="npm run dev"
+alias t="npm test"
+alias b="npm run build"
+alias w="npm run start:watch"
+alias i="npm i"
+alias patch="npm version patch"
+alias s="npm run serve"
+`
+
+const zshrcPath = path.resolve(process.env.HOME, '.zshrc')
+const zshrc = fs.readFileSync(zshrcPath, 'utf8')
+
+fs.writeFileSync(zshrcPath, zshrc + aliases)
+
+execSync(`source ${zshrcPath}`, { shell: '/bin/zsh' })
